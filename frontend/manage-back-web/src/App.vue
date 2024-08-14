90539d4b2604d011c320d761635e794f9cf54f23
yuanaolin <yuanaolin@dianxiaomi.com>
2024-08-14 16:54:13 +0800

    feat:测试nx 增量构建

--- frontend/manage-back-web/src/App.vue
+++ frontend/manage-back-web/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>App</div>
   <Test></Test>
+  <router-link to="/">home</router-link> &nbsp;
+  <router-link to="/about">about</router-link>
+  <router-view></router-view>
 </template>
 
 <script setup lang="ts">
 import { Test } from '@xigua/components';
-import { sum } from '@xigua/utils'
+import { sum } from '@xigua/utils';
 console.log(sum(1, 2));
 </script>
 
---
+++ frontend/manage-back-web/src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>home page change111</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
