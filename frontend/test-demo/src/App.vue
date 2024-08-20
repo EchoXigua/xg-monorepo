cb7da4795c1233629a2df7054978ea1d4853eae6
echoxigua <643589668@qq.com>
2024-08-20 18:48:33 +0800

    feat：测试release

--- frontend/test-demo/src/App.vue
+++ frontend/test-demo/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>test demo project build test~~~</div>
-  <div>demo 项目 测试 nx release 功能</div>
+  <div>测试 nx release</div>
   <img src="/vite.svg" class="logo" alt="Vite logo" />
   <HelloWorld msg="Vite + Vue" />
 </template>
