30a6b873686307fc166322551a74e7ee7cdca5a3
echoxigua <643589668@qq.com>
2024-08-20 11:10:55 +0800

    feat:test release

--- frontend/test-demo/src/App.vue
+++ frontend/test-demo/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>test demo project build test~~~</div>
+  <div>test release</div>
   <img src="/vite.svg" class="logo" alt="Vite logo" />
   <HelloWorld msg="Vite + Vue" />
 </template>
