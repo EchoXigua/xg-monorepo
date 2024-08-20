874b84e1c97bd78987581f6e469196681654ba15
EchoXigua <643589668@qq.com>
2024-08-21 00:03:13 +0800

    feat: test 项目版本更新

--- frontend/test-demo/src/App.vue
+++ frontend/test-demo/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>test demo project build test~~~</div>
-  <div>测试 每个子项目 独立 更新版本</div>
+  <div>test 项目独立 更新版本</div>
   <img src="/vite.svg" class="logo" alt="Vite logo" />
   <HelloWorld msg="Vite + Vue" />
 </template>
