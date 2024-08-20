1be9484863edd9a71badfd03d354a8357b6337b2
echoxigua <643589668@qq.com>
2024-08-20 20:36:42 +0800

    feat: 测试子项目更新版本号

--- frontend/test-demo/src/App.vue
+++ frontend/test-demo/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>test demo project build test~~~</div>
-  <div>测试 nx release 成功运行</div>
+  <div>测试 每个子项目 独立 更新版本</div>
   <img src="/vite.svg" class="logo" alt="Vite logo" />
   <HelloWorld msg="Vite + Vue" />
 </template>
