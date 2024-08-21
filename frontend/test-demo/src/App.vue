b0307ef84fc2007c9da503c35f937510f295366a
echoxigua <643589668@qq.com>
2024-08-21 10:16:19 +0800

    feat: test-demo version

--- frontend/test-demo/src/App.vue
+++ frontend/test-demo/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>test demo project build test~~~</div>
-  <div>123</div>
+  <div>test-demo version : 1.2.5</div>
   <img src="/vite.svg" class="logo" alt="Vite logo" />
   <HelloWorld msg="Vite + Vue" />
 </template>
