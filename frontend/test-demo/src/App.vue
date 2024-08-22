74e460e865469f53224ba258564b28221c81085e
echoxigua <643589668@qq.com>
2024-08-22 10:24:30 +0800

    feat: test ci push protect branches

--- frontend/test-demo/src/App.vue
+++ frontend/test-demo/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>test demo project build test~~~</div>
   <div>子项目独立版本更新</div>
+  <div>测试ci 版本变更到受保护分支</div>
   <img src="/vite.svg" class="logo" alt="Vite logo" />
   <HelloWorld msg="Vite + Vue" />
 </template>
