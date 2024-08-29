7309fa0d7dce761d0d59327d086302afeaacef13
echoxigua <643589668@qq.com>
2024-08-29 09:19:17 +0800

    feat: 调试sdk

--- sdk/vue/src/sdk.ts
+++ sdk/vue/src/sdk.ts
@@ -22,20 +22,24 @@ export function init(
 ): Client | undefined {
   const options = {
     _metadata: {
+      // _metadata 包含有关 SDK 的信息，主要用于内部跟踪和诊断。
       sdk: {
-        name: 'sentry.javascript.vue',
+        name: 'xigua.javascript.vue',
         packages: [
           {
-            name: 'npm:@sentry/vue',
+            name: 'npm:@xigua-monitor/vue',
             version: SDK_VERSION,
           },
         ],
         version: SDK_VERSION,
       },
     },
+    //  包含了 Sentry SDK 的默认集成，这些集成是 Sentry 运行所必需的。
     defaultIntegrations: [...getDefaultIntegrations(config), vueIntegration()],
+    // 覆盖默认配置
     ...config,
   };
 
+  //  Sentry SDK 的核心初始化逻辑之一，负责启动 Sentry 客户端，开始捕获错误和追踪性能。
   return browserInit(options);
 }
