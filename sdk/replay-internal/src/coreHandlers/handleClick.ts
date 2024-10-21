a865f5220910138be39193572cd8db6cbff2a0ad
echoxigua <643589668@qq.com>
2024-10-21 17:00:33 +0800

    feat: handle click

---
+++ sdk/replay-internal/src/coreHandlers/handleClick.ts
@@ -0,0 +1,12 @@
+import type {
+    RecordingEvent,
+    ReplayClickDetector,
+    ReplayContainer,
+    ReplayMultiClickFrame,
+    ReplaySlowClickFrame,
+    SlowClickConfig,
+  } from '../types';
+
+export class ClickDetector implements ReplayClickDetector {
+
+}
